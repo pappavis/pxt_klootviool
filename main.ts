5a2d60ecba26fb53a72c60c58ba71e739e892152
Michiel Erasmus <swartskaap@gmail.com>
2018-07-30 21:48:33 +0200

    Eerste commit

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ enum PingUnit {
 //% color="#2c3e50" weight=10
 namespace KLOOTVIOOL_XY2 {
     /**
-     * Stuur een KLOOTVIOOL_XY1 pingen meet die echo tijd (in microseconden) als resultaat
+     * Stuur een KLOOTVIOOL_XY2 pingen meet die echo tijd (in microseconden) als resultaat
      * @param trig tigger pin pennetje
      * @param echo echo pin
      * @param teller1 lekker onzin praat
