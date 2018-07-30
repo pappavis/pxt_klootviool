6cddfcd74c663f983d3bf963c1302c96cdf6784e
Michiel Erasmus <swartskaap@gmail.com>
2018-07-30 22:10:50 +0200

    Eerste commit

--- main.ts
+++ main.ts
@@ -20,5 +20,23 @@ namespace KLOOTVIOOL_XY2 {
      * @param unit desired omreken eenheid
      * @param maxCmDistance maximum afstand in centimeter (default is 500)
      */
+    //% blockId=KLOOTVIOOL_XY2 block="ping trig %trig|echo %echo|unit %unit"
+    export function ping(trig: AnalogPin, echo: AnalogPin, unit: PingUnit, maxCmDistance : number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
 
+        // read pulse
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.idiv(d, 58);
+            case PingUnit.Inches: return Math.idiv(d, 148);
+            default: return d ;
+        }
+    }
 }
