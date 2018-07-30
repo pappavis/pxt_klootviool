a8f2482d0ab4437b6c28cbe0fa7dbb4b6640093a
Michiel Erasmus <swartskaap@gmail.com>
2018-07-30 22:09:11 +0200

    Eerste commit

--- main.ts
+++ main.ts
@@ -8,10 +8,17 @@ enum PingUnit {
 }
 
 /**
- * XY2 and ping nuts
+ * KLOOTVIOOL_XY2 and ping utilities
  */
 //% color="#2c3e50" weight=10
-namespace XY2 {
-
+namespace KLOOTVIOOL_XY2 {
+    /**
+     * Stuur een KLOOTVIOOL_XY2 pingen meet die echo tijd (in microseconden) als resultaat
+     * @param trig tigger pin pennetje
+     * @param echo echo pin
+     * @param teller1 lekker onzin praat
+     * @param unit desired omreken eenheid
+     * @param maxCmDistance maximum afstand in centimeter (default is 500)
+     */
 
 }
